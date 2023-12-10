be11ad97f0dafd2476f5faf959ee8c5364b7655a
yhiriend <yirien45@gmail.com>
2023-12-10 01:35:31 -0500

    update prod env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
-  authApi: 'http://localhost:3001/auth/users',
-  productApi: 'http://localhost:3001/api/products',
+  authApi: 'http://3.149.254.42/auth/users',
+  productApi: 'http://3.149.254.42/api/products',
+  cartApi: 'http://3.149.254.42/api/cart',
+  reviewRequestApi: 'http://3.149.254.42/api/reviewrequest',
+  serviceRequestApi: 'http://3.149.254.42/api/servicerequest',
   secretKey: 'T9$9CZPadt@t@I6AXjRB5PHbchDFyL$YrC%h&%UL!nx&!6+hNpx5IQSa7fW8',
 };
